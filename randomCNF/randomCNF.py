f161516ca7a98a3ad2493983f3e379de7a98a4e5
Ziwei Huang <z.huangcsee@gmail.com>
2017-10-24 10:16:22 +0200

    fix the output format to match examples, save the output in files

--- randomCNF/randomCNF.py
+++ randomCNF/randomCNF.py
@@ -1,21 +1,35 @@
 import random
 
-# the return example is like 
-# ['p', 'cnf', 5, 3, 1, 2, -3, 5, 0, 3, -5, 0, 3, 0]
+def randomCFN(n_variables, n_clauses):
+    s = list()
+    s = s + ['p', 'cnf', str(n_variables), str(n_clauses)]
+    for i in range(0, n_clauses):
+        for j in range(1, n_variables + 1):
+            if random.randint(0, 1) == 0:
+                n = ((-1) ** (random.randint(0, 1))) * j
+                s = s + [str(n)]
+        s = s + [str(0)]
+    return s
+
+# random.seed(2)
+n_variables = 5
+n_clauses = 3
+s = randomCFN(n_variables, n_clauses)
+print(s)
+
+file = open("sampleCNF.cnf", "w")
+i = 1
+for ele in s:
+    if i <= 3:
+        file.write(ele + ' ')
+        i = i + 1
+    elif i == 4:
+        file.write(ele + '\n')
+        i = i +1
+    elif ele != '0':
+        file.write(ele+' ')
+    else:
+        file.write(ele+'\n')
+file.close()
 
 
-def randomCFN( n_variables, n_clauses):
-
-	S = list()
-	S = S + ['p', 'cnf', n_variables, n_clauses]
-	print (S)
-	for i in range(0, n_clauses):
-		for j in range(1,n_variables+1):
-			if random.randint(0,1) == 0:
-				n = ((-1)**(random.randint(0,1)))*j
-				S = S + [n]
-		S = S + [0]
-	return S
-
-#random.seed(2)
-#print(randomCFN(5, 3))
