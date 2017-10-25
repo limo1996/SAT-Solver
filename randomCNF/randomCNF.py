90c7c26883fb85a83f6cb49a9c72510301f0301d
Ziwei Huang <z.huangcsee@gmail.com>
2017-10-25 13:15:39 +0200

    test

--- randomCNF/randomCNF.py
+++ randomCNF/randomCNF.py
@@ -1,40 +1,37 @@
 import random
 
-def randomCFN(n_variables, n_clauses):
-    s = list()
-    s = s + ['p', 'cnf', str(n_variables), str(n_clauses)]
-    for i in range(0, n_clauses):
-        s_len_pre = len(s)
-        for j in range(1, n_variables + 1): # randomly choose each variable in a clause
-            if random.randint(0, 1) == 0:
-                n = ((-1) ** (random.randint(0, 1))) * j
+class CNFGenerator(object)
+    def __init__(self, n_variables, n_clauses):
+        self.n_variables = n_variables
+        self.n_clauses = n_clauses
+
+    def randomCNF(self):
+        s = list()
+        s = s + ['p', 'cnf', str(n_variables), str(n_clauses)]
+        for i in range(0, n_clauses):
+            s_len_pre = len(s)
+            for j in range(1, n_variables + 1): # randomly choose each variable in a clause
+                if random.randint(0, 1) == 0:
+                    n = ((-1) ** (random.randint(0, 1))) * j
+                    s = s + [str(n)]
+            if s_len_pre == len(s): # if no variable is slected in the preious step, force to have at lease one variable in the line
+                n = ((-1) ** (random.randint(0, 1))) * random.randint(1,n_variables)
                 s = s + [str(n)]
-        if s_len_pre == len(s): # if no variable is slected in the preious step, force to have at lease one variable in the line
-            n = ((-1) ** (random.randint(0, 1))) * random.randint(1,n_variables)
-            s = s + [str(n)]        
-        s = s + [str(0)]
-    return s
-
-# random.seed(2)
-#n_variables = 2
-#n_clauses = 3
-
-s = randomCFN(n_variables, n_clauses)
-print(s)
-
-file = open("sampleCNF.cnf", "w")
-i = 1
-for ele in s:
-    if i <= 3:
-        file.write(ele + ' ')
-        i = i + 1
-    elif i == 4:
-        file.write(ele + '\n')
-        i = i +1
-    elif ele != '0':
-        file.write(ele+' ')
-    else:
-        file.write(ele+'\n')
-file.close()
+            s = s + [str(0)]
+        print(s)
+        file = open("sampleCNF.cnf", "w")
+        i = 1
+        for ele in s:
+            if i <= 3:
+                file.write(ele + ' ')
+                i = i + 1
+            elif i == 4:
+                file.write(ele + '\n')
+                i = i +1
+            elif ele != '0':
+                file.write(ele+' ')
+            else:
+                file.write(ele+'\n')
+        file.close()
 
 
--- randomCNF/testFolder.py
+++ randomCNF/testFolder.py
@@ -1,6 +1,11 @@
-from test import Tester
+# from test import Tester
 
+# tester = Tester("/home/ziwei/DPHPC/SAT-Solver/randomCNF/examples")
+# tester.run_test()
 
+from randomCNF import CNFGenerator
 
-tester = Tester("/home/ziwei/DPHPC/SAT-Solver/randomCNF/examples")
-tester.run_test()
+cnfGenerator = CNFGenerator()
+cnfGenerator.n_variables = 3
+cnfGenerator.n_clauses = 5
+cnfGenerator.randomCNF()
