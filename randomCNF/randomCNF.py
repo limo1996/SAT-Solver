f9bf110fd548a2697db8b655ba3977a77249010c
ziwei <ziwei@student.chalmers.se>
2017-10-25 11:14:50 +0200

    add comment

--- randomCNF/randomCNF.py
+++ randomCNF/randomCNF.py
@@ -4,12 +4,12 @@ def randomCFN(n_variables, n_clauses):
     s = list()
     s = s + ['p', 'cnf', str(n_variables), str(n_clauses)]
     for i in range(0, n_clauses):
-        s_len_1 = len(s)
-        for j in range(1, n_variables + 1):
+        s_len_pre = len(s)
+        for j in range(1, n_variables + 1): # randomly choose each variable in a clause
             if random.randint(0, 1) == 0:
                 n = ((-1) ** (random.randint(0, 1))) * j
                 s = s + [str(n)]
-        if s_len_1 == len(s):
+        if s_len_pre == len(s): # if no variable is slected in the preious step, force to have at lease one variable in the line
             n = ((-1) ** (random.randint(0, 1))) * random.randint(1,n_variables)
             s = s + [str(n)]        
         s = s + [str(0)]
