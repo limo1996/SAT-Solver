e57c52244998835717ac062b5bb2ac02c4de4947
ziwei <ziwei@student.chalmers.se>
2017-10-30 15:28:08 +0100

    test

--- randomCNF/test.py
+++ randomCNF/test.py
@@ -6,26 +6,37 @@ from z3 import Solver, Not, And
 from cnf_parser import CnfParser
 
 
+class SolverError(Exception):
+    pass
+
+
 class Tester(object):
-    def __init__(self, folder):
+    def __init__(self, folder, parallel):
         folder = os.path.join(os.pardir, folder)
         self.folder = folder
         self.files = [os.path.join(folder, f)
                       for f in os.listdir(folder)
                       if os.path.isfile(os.path.join(folder, f))]
         self.cnf_parser = CnfParser()
+        self.parallel = parallel
         self.solver = self.compile_and_create_solver()
         self.fail_count = 0
 
-    @staticmethod
-    def compile_and_create_solver():
-        cpp_path = os.path.join(os.pardir, 'dummy_solver.cpp')
-        cpp_o_path = os.path.join(os.pardir, 'solver.cpp.o')
-        command = 'g++ --std=c++11 {0} -o {1}'.format(cpp_path, cpp_o_path)
-        ret = subprocess.call(command, shell=True)
+    def compile_and_create_solver(self):
+        print('compiling...')
+        cmake = 'cmake ' + os.pardir + '> /dev/null'
+        make = 'make -C ' + os.pardir + '> /dev/null'
+        ret = subprocess.call(cmake, shell=True)
         if ret != 0:
-            raise RuntimeError("c++ compilation failed!")
-        return ParallelSolver()
+            raise RuntimeError("cmake failed!")
+        ret = subprocess.call(make, shell=True)
+        if ret != 0:
+            raise RuntimeError("make failed!")
+        print('compilation done!')
+        if self.parallel:
+            return ParallelSolver()
+        else:
+            return SequentialSolver()
 
     def print_file_list(self):
         for f in self.files:
@@ -38,10 +49,15 @@ class Tester(object):
             s = Solver()
             s.add(cnf.z3_formula)
             result = s.check().r
-            if result is 1:
-                self.handle_sat_case(f, cnf)
-            else:
-                self.handle_unsat_case(f)
+            try:
+                if result is 1:
+                    self.handle_sat_case(f, cnf)
+                else:
+                    self.handle_unsat_case(f)
+            except SolverError:
+                print('[crash] {0}'.format(f))
+                self.fail_count += 1
+
         print('')
         correct_count = len(self.files) - self.fail_count
         count = len(self.files)
@@ -53,11 +69,12 @@ class Tester(object):
         output_correct = correct_num_vars and output[0].startswith('sat')
         if not output_correct:
             print('[fail]   {0}'.format(input_file))
+            self.fail_count += 1
         else:
             if self._inputs_correct(output[1:], cnf):
-                print('[ok]   {0}'.format(input_file))
+                print('[ok]    {0}'.format(input_file))
             else:
-                print('[fail] {0}'.format(input_file))
+                print('[fail]  {0}'.format(input_file))
                 self.fail_count += 1
 
     def _inputs_correct(self, input_lines, cnf):
@@ -83,19 +100,18 @@ class Tester(object):
         output = self.solver.solve(input_file)
         output_correct = len(output) == 1 and output[0].startswith('unsat')
         if output_correct:
-            print('[ok]   {0}'.format(input_file))
+            print('[ok]    {0}'.format(input_file))
         else:
-            print('[fail] {0}'.format(input_file))
+            print('[fail]  {0}'.format(input_file))
             self.fail_count += 1
 
 
-class ParallelSolver(object):
+class SequentialSolver(object):
     def __init__(self):
-        self.num_cores = 1
         cwd = os.path.join(os.getcwd(), os.path.pardir)
-        self.executable = os.path.join(cwd, 'solver.cpp.o')
+        self.executable = os.path.join(cwd, 'sequential_main')
         if not os.path.exists(self.executable):
-            raise ValueError('The executable solver.cpp.o does not exist!')
+            raise ValueError('The executable sequential_main does not exist!')
 
     def solve(self, input_file):
         """ Invokes the solver for a given input file
@@ -103,11 +119,21 @@ class ParallelSolver(object):
         :param input_file: the path to the input file
         :return: a list of lines that the solver did output to std_out
         """
-        command = '{0} < {1} > out'.format(self.executable, input_file)
-        ret = subprocess.call(command, shell=True)
+        command = '{0} {1} -p 2 > out'.format(self.executable, input_file)
+        ret = subprocess.call(command, shell=True,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT)
         if ret != 0:
-            # return the error code if the solver crashed
-            return ret
+            raise SolverError("Solver did not return 0")
 
         f = open('out', 'r')
         return [line for line in f]
+
+
+class ParallelSolver(SequentialSolver):
+    def __init__(self):
+        self.num_cores = 1
+        cwd = os.path.join(os.getcwd(), os.path.pardir)
+        self.executable = os.path.join(cwd, 'parallel_main')
+        if not os.path.exists(self.executable):
+            raise ValueError('The executable parallel_main does not exist!')
