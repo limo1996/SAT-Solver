112d332005548fd5f9a2b06221a5ca1508c7f337
ziwei <ziwei@student.chalmers.se>
2017-10-31 12:38:01 +0100

    split normal Cnf generator and generator with probability

--- randomCNF/testCnfPro.py
+++ randomCNF/testCnfPro.py
@@ -10,31 +10,32 @@ if os.path.exists(path):
 	shutil.rmtree(path)
 
 pro = 0 # initial pro
+n_variables = 3
+n_clauses = 5
+n_examples = 10
+
 
 # generate examples with 0.5 sat
-# while pro != 0.5:
-# 	# n_variables, n_clauses, examples, path
-# 	# to get pro = 0.5 fast, consider n_clauses a bit larger than n_clauses
-# 	# it is always good to see the first few probabilities (print out), to have a general idea on 
-# 	# parameters setting
-# 	# for example, if set 5, 10, 100, the most probabilies seem are above 0.5
-# 	# change to 5, 14, 100, the probabilites are around 0.5, good!
-# 	# n_variables, n_clauses, examples, path
-# 	cnfGenerator = CNFGenerator(5, 10, 100, path) 
-# 	cnfGenerator.saveCNF()	
-# 	tester = testCNF(path)	
-# 	(count, count_file) = tester.run_test()
-# 	tester.print_result()
-# 	pro = float(count) / count_file
-# 	print(pro)
+while pro != 0.5:
+ 	# n_variables, n_clauses, examples, path
+ 	# to get pro = 0.5 fast, consider n_clauses a bit larger than n_clauses
+ 	# it is always good to see the first few probabilities (print out), to have a general idea on 
+ 	# parameters setting
+ 	# for example, if set 5, 10, 100, the most probabilies seem are above 0.5
+ 	# change to 5, 14, 100, the probabilites are around 0.5, good!
+ 	# n_variables, n_clauses, examples, path
+ 	cnfGenerator = CNFGenerator(n_variables, n_clauses, n_examples, path) 
+ 	cnfGenerator.saveCNF()	
+ 	tester = testCNF(path)	
+ 	(count, count_file) = tester.run_test()
+ 	tester.print_result()
+ 	pro = float(count) / count_file
+ 	print(pro)
 
-
-# generate examples without considering probability of sat
-# n_variables, n_clauses, examples, path
-cnfGenerator = CNFGenerator(5, 10, 1000, path) 
-cnfGenerator.saveCNF()
+# check sequential result with z3 solver
 foldertester = Tester(path, False)
 foldertester.run_test()
+
 
 
 # example output, with parameters 3, 5, 10
