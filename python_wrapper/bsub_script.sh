f39b89b96feb10e6b2c8c06f2e36016a52d0d255
ebhardjan <ebhardjan@gmail.com>
2017-11-04 21:43:19 +0100

    adding smaller more finegrained 3-sat test-set

--- python_wrapper/bsub_script.sh
+++ python_wrapper/bsub_script.sh
@@ -11,7 +11,7 @@ FILES=*.cnf
 
 for file in ${FILES}
 do
-    for run in 1 2 3 4
+    for run in 1 2 3 4 5 6 7 8 9 10
     do
         ../sequential_main "$file" -p 2 > "$file".out
     done
@@ -21,7 +21,7 @@ for nodes in ${num_nodes}
 do
     for file in ${FILES}
     do
-        for run in 1 2 3 4
+        for run in 1 2 3 4 5 6 7 8 9 10
         do
             mpirun -np "$nodes" ../parallel_main "$file" -p 2 > "$file".out
         done
