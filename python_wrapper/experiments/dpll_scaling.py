c486132dabf13b917e62a9e8c77ff0ec3c388530
ebhardjan <ebhardjan@gmail.com>
2018-01-10 18:45:53 +0100

    comm. figure, compressing figures and writing comm section

--- python_wrapper/experiments/dpll_scaling.py
+++ python_wrapper/experiments/dpll_scaling.py
@@ -11,13 +11,20 @@ from utils import get_netz_username, parse_into_dict, conf_95_mean
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from euler import EulerTester
 
-NAME_MAP = {'parallel': 'Parallel', 'stealing': 'Work Stealing'}
+NAME_MAP = {'parallel': 'Master Worker', 'stealing': 'Work Stealing'}
+NAME_POST_FIX = 'Model'
 COLORS = ['teal', 'firebrick', 'darkorange', 'royalblue',
           'palevioletred', 'slateblue', 'black', 'grey']
 OFFSETS = [-0.25, 0.25, 0, 0.125, -0.125,
            0.25, -0.25, 0, 0.125, -0.125,
            -0.25, 0.25, 0, 0.125, -0.125]
 MARKERS = ['o', 'd', 's', '^', 'x', 'D', 'v', '.']
+
+MEW = 1.75
+MS = 6
+FIG_WIDTH = 6
+FIG_HEIGHT = 3.4
+FIG_HEIGHT_SMALL = 2.5
 
 
 class DpllScaling(AbstractExperiment):
@@ -54,24 +61,31 @@ class DpllScaling(AbstractExperiment):
 
         #self.runtime_plots(subset, 'subset')
         #self.runtime_plots(non_subset, 'non_subset')
-        #self.speedup_plots(subset, 'subset')
-        #self.speedup_plots(non_subset, 'non_subset')
-        #self.waiting_plots(subset, 'subset')
-        #self.waiting_plots(non_subset, 'non_subset')
+        self.speedup_plots(subset, 'subset')
+        self.speedup_plots(non_subset, 'non_subset')
+        self.waiting_plots(subset, 'subset')
+        self.waiting_plots(non_subset, 'non_subset')
+        self.comm_plots(subset, 'subset')
+        self.comm_plots(non_subset, 'non_subset')
         self.find_best_runtime(all_files)
+
+    def _default_plt_postprocessing(self):
+        plt.tight_layout(pad=0)
+        plt.grid('grid', color='whitesmoke', linestyle='-')
+
 
     def runtime_plots(self, set, key):
         tar = self.data['tar'].replace('.', '_')
         for p in ['parallel', 'stealing']:
-            figure = plt.figure(figsize=(6, 4))
+            figure = plt.figure(figsize=(FIG_WIDTH, FIG_HEIGHT))
             ax = figure.add_subplot(1, 1, 1)
             for f in range(len(set)):
                 self._plot_runtime(self.data[set[f]], p, ax,
                                    color=COLORS[f], offset=OFFSETS[f],
                                    marker=MARKERS[f], legend=set[f])
-            plt.title('Runtime - {} Version'.format(NAME_MAP[p]))
+            plt.title('Runtime - {} {}'.format(NAME_MAP[p], NAME_POST_FIX))
             plt.xlabel('# cores')
-            plt.ylabel('avg. runtime [ms]')
+            plt.ylabel('Avg. runtime [ms]')
             plt.legend()
             plt.tight_layout(pad=0)
             f = '../../report/figures/runtime_{}_{}_{}.pdf'.format(p, key, tar)
@@ -101,16 +115,16 @@ class DpllScaling(AbstractExperiment):
     def speedup_plots(self, set, key):
         tar = self.data['tar'].replace('.', '_')
         for p in ['parallel', 'stealing']:
-            figure = plt.figure(figsize=(6, 4))
+            figure = plt.figure(figsize=(FIG_WIDTH, FIG_HEIGHT))
             ax = figure.add_subplot(1, 1, 1)
             for f in range(len(set)):
                 self._plot_speedup(self.data[set[f]], p, ax,
                                    color=COLORS[f], marker=MARKERS[f],
                                    offset=OFFSETS[f], plotline=(f == 0),
                                    legend=set[f])
-            plt.title('Speedup - {} Version'.format(NAME_MAP[p]))
+            plt.title('Speedup - {} {}'.format(NAME_MAP[p], NAME_POST_FIX))
             plt.xlabel('# cores')
-            plt.ylabel('speedup')
+            plt.ylabel('Speedup')
             handles, labels = ax.get_legend_handles_labels()
 
             if p == 'parallel':
@@ -121,7 +135,7 @@ class DpllScaling(AbstractExperiment):
             l = labels.pop(0)
             labels.append(l)
             plt.legend(handles, labels)
-            plt.tight_layout(pad=0)
+            self._default_plt_postprocessing()
             f = '../../report/figures/scaling_{}_{}_{}.pdf'.format(p, key, tar)
             plt.savefig(f, format='pdf')
             f = '../../report/figures/scaling_{}_{}_{}.png'.format(p, key, tar)
@@ -130,26 +144,65 @@ class DpllScaling(AbstractExperiment):
     def waiting_plots(self, set, key):
         tar = self.data['tar'].replace('.', '_')
         for p in ['parallel', 'stealing']:
-            figure = plt.figure(figsize=(6, 4))
+            figure = plt.figure(figsize=(FIG_WIDTH, FIG_HEIGHT_SMALL))
             ax = figure.add_subplot(1, 1, 1)
             for f in range(len(set)):
                 self._plot_waiting(self.data[set[f]], p, ax,
                                    color=COLORS[f], marker=MARKERS[f],
                                    offset=OFFSETS[f], label=set[f])
-            plt.title('Overall Waiting Time per Formula - {} Version'.format(
-                NAME_MAP[p]))
+            plt.title('Overall Waiting Time per Formula - {} {}'.format(
+                NAME_MAP[p], NAME_POST_FIX))
             plt.xlabel('# cores')
-            plt.ylabel('avg. overall waiting time [ms]')
+            plt.ylabel('Avg. overall waiting time [s]')
             handles, labels = ax.get_legend_handles_labels()
             if p == 'parallel':
                 handles = [handles[0]] + [handles[-1]] + handles[1:-1]
                 labels = [labels[0]] + [labels[-1]] + labels[1:-1]
             plt.legend(handles, labels)
-            plt.tight_layout(pad=0)
+            self._default_plt_postprocessing()
             f = '../../report/figures/waiting_{}_{}_{}.pdf'.format(p, key, tar)
             plt.savefig(f, format='pdf')
             f = '../../report/figures/waiting_{}_{}_{}.png'.format(p, key, tar)
             plt.savefig(f, format='png')
+
+    def comm_plots(self, set, key):
+        tar = self.data['tar'].replace('.', '_')
+        figure = plt.figure(figsize=(FIG_WIDTH, 6))
+        axs = {'parallel': figure.add_subplot(2, 1, 1),
+               'stealing': figure.add_subplot(2, 1, 2)}
+        for p in ['parallel', 'stealing']:
+            ax = axs[p]
+            for f in range(len(set)):
+                self._plot_comm(self.data[set[f]], p, ax,
+                                color=COLORS[f], marker=MARKERS[f],
+                                offset=OFFSETS[f], label=set[f])
+            ax.set_title('Communication - {} {}'.format(
+                NAME_MAP[p], NAME_POST_FIX))
+            ax.set_xlabel('# cores')
+            ax.set_ylabel('Avg. communication [bytes]')
+            handles, labels = ax.get_legend_handles_labels()
+            if p == 'parallel':
+                handles = [handles[0]] + [handles[-1]] + handles[1:-1]
+                labels = [labels[0]] + [labels[-1]] + labels[1:-1]
+            if p == 'parallel':
+                ax.set_ylim(-2500, 100000)
+            else:
+                ax.set_ylim(-100, 8000)
+            yticks = ax.get_yticks()[1:]
+            ytick_labels = list(map(lambda t: str(t/1000)[:-2] + ' k', yticks))
+            ax.set_yticks(yticks)
+            ax.set_yticklabels(ytick_labels)
+            if p == 'parallel':
+                ax.set_ylim(-2500, 100000)
+            else:
+                ax.set_ylim(-100, 8000)
+            ax.legend(handles, labels)
+            ax.grid('grid', color='whitesmoke', linestyle='-')
+        plt.tight_layout(pad=0, h_pad=1)
+        f = '../../report/figures/comm_{}_{}.pdf'.format(key, tar)
+        plt.savefig(f, format='pdf')
+        f = '../../report/figures/comm_{}_{}.png'.format(key, tar)
+        plt.savefig(f, format='png')
 
     def _single_formula_runtime_plot(self, parallel_key):
         for f, v in self.data.items():
@@ -159,7 +212,7 @@ class DpllScaling(AbstractExperiment):
             self._plot_runtime(v, parallel_key, figure)
             plt.title('{}.cnf'.format(f))
             plt.xlabel('# cores')
-            plt.ylabel('avg total waiting time [ms]')
+            plt.ylabel('Avg total waiting time [ms]')
             plt.savefig('figures/{}_{}.png'.format(f, parallel_key))
 
     def _plot_runtime(self, data, parallel_key, figure, color='teal',
@@ -187,7 +240,8 @@ class DpllScaling(AbstractExperiment):
         ys = [y for _, y in sorted(zip(xs, ys))]
         xs = sorted(xs)
         figure.errorbar(xs, ys, yerr=[lowers, uppers], fmt=marker,
-                        markerfacecolor='none', color=color, label=legend)
+                        markerfacecolor='none', color=color, label=legend,
+                        mew=MEW, ms=MS)
         plt.plot(xs, ys, '-', alpha=0.5, linewidth=0.5, color=color)
 
     def _single_formula_speedup_plot(self, parallel_key):
@@ -198,7 +252,7 @@ class DpllScaling(AbstractExperiment):
             self._plot_speedup(v, parallel_key, figure)
             plt.title('{}.cnf'.format(f))
             plt.xlabel('# cores')
-            plt.ylabel('speedup')
+            plt.ylabel('Speedup')
             plt.savefig('figures/{}_speedup_{}.png'.format(f, parallel_key))
 
     def _plot_speedup(self, data, parallel_key, figure, color='teal',
@@ -224,11 +278,13 @@ class DpllScaling(AbstractExperiment):
             xs = list(map(lambda x: float(x) + offset, xs))
             if legend == '':
                 figure.errorbar(xs, ys, yerr=[lowers, uppers], fmt=marker,
-                                markerfacecolor='none', color=color)
+                                markerfacecolor='none', color=color,
+                                mew=MEW, ms=MS)
             else:
                 figure.errorbar(xs, ys, yerr=[lowers, uppers], fmt=marker,
                                 markerfacecolor='none', color=color,
-                                label=legend)
+                                label=legend,
+                                mew=MEW, ms=MS)
             plt.plot(xs, ys, '-', alpha=0.5, linewidth=0.5, color=color)
             if plotline:
                 plt.plot([0, max(xs)], [0, max(xs)], '-', color='black',
@@ -246,6 +302,7 @@ class DpllScaling(AbstractExperiment):
             xs.append(int(cores) + offset)
             sums = list(map(lambda wt: np.sum(np.array(wt[1:])),
                              v_['wait']))
+            sums = np.array(sums) / 1000
             mean = np.mean(sums)
             ys.append(mean)
             lower, upper = conf_95_mean(sums)
@@ -256,8 +313,33 @@ class DpllScaling(AbstractExperiment):
         uppers = [u for _, u in sorted(zip(xs, uppers))]
         xs = sorted(xs)
         figure.errorbar(xs, ys, yerr=[lowers, uppers], fmt=marker,
-                        markerfacecolor='none', color=color, label=label)
+                        markerfacecolor='none', color=color, label=label,
+                        mew=MEW, ms=MS)
         plt.plot(xs, ys, '-', alpha=0.5, linewidth=0.5, color=color)
+
+    def _plot_comm(self, data, parallel_key, figure, color='teal',
+                   offset=0.0, label='', marker='o'):
+        if parallel_key not in data:
+            return
+        xs, ys, lowers, uppers = [], [], [], []
+        for cores, v_ in data[parallel_key].items():
+            xs.append(int(cores) + offset)
+            sums = list(map(lambda wt: np.sum(np.array(wt[1:])),
+                            v_['comm']))
+            mean = np.mean(sums)
+            ys.append(mean)
+            lower, upper = conf_95_mean(sums)
+            lowers.append(mean - lower)
+            uppers.append(upper - mean)
+        ys = [y for _, y in sorted(zip(xs, ys))]
+        lowers = [l for _, l in sorted(zip(xs, lowers))]
+        uppers = [u for _, u in sorted(zip(xs, uppers))]
+        xs = sorted(xs)
+        figure.errorbar(xs, ys, yerr=[lowers, uppers], fmt=marker,
+                        markerfacecolor='none', color=color, label=label,
+                        mew=MEW, ms=MS)
+        figure.plot(xs, ys, '-', alpha=0.5, linewidth=0.5, color=color)
+
 
 @click.command()
 @click.option('--rerun/--no-rerun', default=False,
